142d82c742e63c608eb5b90fb5bdbae90d3ae522
Richard Adams <ra22597@gmail.com>
2020-05-18 22:41:16 +0100

    format archive output

--- cmd/archive.go
+++ cmd/archive.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
@@ -16,8 +17,14 @@ import (
 var archiveCmd = &cobra.Command{
 	Use:   "archive",
 	Short: "Utility for inspecting RSpace XML archives",
-	Long:  `Inspect and summarise XML archives without opening or importing into RSpace`,
-	Args:  cobra.MinimumNArgs(1),
+	Long: `Inspect and summarise XML archives and their manifest without opening or importing into RSpace.
+
+--summary argument calculates:
+
+- the total number of documents
+- the document creators
+- the date range of documents created in the archive`,
+	Args: cobra.MinimumNArgs(1),
 	Example: `
 // show manifest
 rspace archive myArchive.zip --manifest
@@ -27,12 +34,48 @@ rspace archive myArchive.zip --summary
 	`,
 
 	Run: func(cmd *cobra.Command, args []string) {
+		ctx := initialiseContext()
 		summaries, err := inspectArchives(args, &archiveArgsA)
 		if err != nil {
 			exitWithErr(err)
 		}
-		fmt.Println(summaries)
+		if archiveArgsA.summaryArg {
+			ctx.writeResult(&zipSummaryFormatter{&zipSummaryList{summaries}})
+		}
 	},
+}
+
+type zipSummaryList struct {
+	SummaryList []*zipSummary
+}
+
+type zipSummaryFormatter struct {
+	results *zipSummaryList
+}
+
+func (zs *zipSummaryFormatter) ToJson() string {
+	return prettyMarshal(zs.results)
+}
+
+func (ds *zipSummaryFormatter) ToQuiet() []identifiable {
+	rows := make([]identifiable, 0)
+	for _, res := range ds.results.SummaryList {
+		rows = append(rows, identifiable{res.FileName})
+	}
+	return rows
+}
+
+func (ds *zipSummaryFormatter) ToTable() *TableResult {
+	headers := []columnDef{columnDef{"file", 10}, columnDef{"Total Docs", 10},
+		columnDef{"minDate", 22}, columnDef{"maxDate", 22}, columnDef{"Authors", 50}}
+
+	rows := make([][]string, 0)
+	for _, res := range ds.results.SummaryList {
+		data := []string{res.FileName, strconv.Itoa(res.DocCount),
+			res.MinDate.Format(time.RFC3339), res.MaxDate.Format(time.RFC3339), strings.Join(res.Authors, ";")}
+		rows = append(rows, data)
+	}
+	return &TableResult{headers, rows}
 }
 
 type archiveArgs struct {
@@ -55,7 +98,7 @@ func inspectArchives(args []string, config *archiveArgs) ([]*zipSummary, error) 
 			//messageStdErr(fmt.Sprintf("Could not open %s, skipping", file))
 		}
 		if archiveArgsA.manifestArg {
-			//messageStdErr(fmt.Sprintf("Manifest for %s:", file))
+			messageStdErr(fmt.Sprintf("Manifest for %s:", file))
 			bytes, err := showManifest(reader)
 			if err != nil {
 				messageStdErr(err.Error())
@@ -65,7 +108,8 @@ func inspectArchives(args []string, config *archiveArgs) ([]*zipSummary, error) 
 		}
 		if archiveArgsA.summaryArg {
 			//messageStdErr(fmt.Sprintf("Summary for %s:", file))
-			files := parseFiles(reader)
+			files := parseArchiveFiles(reader)
+			files.FileName = filepath.Base(file)
 			zipSummaries = append(zipSummaries, files)
 		}
 	}
@@ -85,7 +129,7 @@ func parseTimestamp(timestamp string) (time.Time, error) {
 	return time.Parse(time.RFC3339, timestamp)
 }
 
-func parseFiles(reader *zip.ReadCloser) *zipSummary {
+func parseArchiveFiles(reader *zip.ReadCloser) *zipSummary {
 	parsedDocs := make([]*xmlDoc, 0)
 	for _, f := range reader.File {
 		fname := filename(f)
@@ -102,10 +146,11 @@ func parseFiles(reader *zip.ReadCloser) *zipSummary {
 }
 
 type zipSummary struct {
-	docCount int
-	minDate  time.Time
-	maxDate  time.Time
-	authors  []string
+	DocCount int
+	MinDate  time.Time
+	MaxDate  time.Time
+	Authors  []string
+	FileName string
 }
 
 func summarise(docs []*xmlDoc) (*zipSummary, error) {
@@ -129,7 +174,7 @@ func summarise(docs []*xmlDoc) (*zipSummary, error) {
 	for k, _ := range authors {
 		uniqueAuthors = append(uniqueAuthors, k)
 	}
-	return &zipSummary{len(docs), minDate, maxDate, uniqueAuthors}, nil
+	return &zipSummary{len(docs), minDate, maxDate, uniqueAuthors, ""}, nil
 }
 
 func filename(file *zip.File) string {
@@ -152,4 +197,6 @@ func init() {
 	// is called directly, e.g.:
 	archiveCmd.Flags().BoolVar(&archiveArgsA.summaryArg, "summary", false, "Show summary of content")
 	archiveCmd.Flags().BoolVar(&archiveArgsA.manifestArg, "manifest", true, "Shows manifest of the archive")
+	archiveCmd.Flags().StringVar(&outputFormatArg, "outputFormat", "table", "Output format: one of 'json','table', 'csv' or 'quiet' ")
+	archiveCmd.Flags().StringVar(&outFileArg, "outFile", "", "Output file for program output")
 }
